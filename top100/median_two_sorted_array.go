ead433a9b0ee64fa33dad277ec5f247080c078b4
ken <pengkunliu@gmail.com>
2019-11-30 15:51:45 +0800

    use tier to solve search words with pattern

--- top100/median_two_sorted_array.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	
-}
---
+++ top100liked/add_and_search_words.go
@@ -0,0 +1,63 @@
+package main
+
+type WordDictionary struct {
+	Children [27]*WordDictionary
+}
+
+func Constructor() WordDictionary {
+	return WordDictionary{}
+}
+
+func (root *WordDictionary) AddWord(word string) {
+	start := root
+	strLen := len(word)
+	for i := 0; i < strLen; i++ {
+		if node := start.Children[word[i]-'a']; node != nil {
+			start = node
+		} else {
+			for i < strLen {
+				next := &WordDictionary{}
+				start.Children[word[i]-'a'] = next
+				start = next
+				i++
+			}
+			break
+		}
+	}
+	if start.Children[26] == nil {
+		start.Children[26] = &WordDictionary{}
+	}
+}
+
+func (root *WordDictionary) SearchHelper(word []rune, ind int) bool {
+	start := root
+	strLen := len(word)
+
+	for i := ind; i < strLen; i++ {
+		r := word[i]
+		if r == '.' {
+			for _, c := range start.Children {
+				if c != nil {
+					if c.SearchHelper(word, i+1) {
+						return true
+					}
+				}
+			}
+			return false
+
+		} else if node := start.Children[r-'a']; node != nil {
+			start = node
+		} else {
+			return false
+		}
+	}
+
+	if start.Children[26] != nil {
+		return true
+	}
+	return false
+}
+
+func (root *WordDictionary) Search(word string) bool {
+	return root.SearchHelper([]rune(word), 0)
+}
